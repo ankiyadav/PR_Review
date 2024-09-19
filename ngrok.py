c01c698d2d52cc69dac232aa4db66c1485c975e8
ankiyadav <47588240+ankiyadav@users.noreply.github.com>
2024-09-19 23:40:42 +0530

    Add files via upload

---
+++ ngrok.py
@@ -0,0 +1,15 @@
+import ngrok
+import time
+
+# Establish connectivity
+listener = ngrok.forward(5000, authtoken_from_env=True)
+
+# Output ngrok url to console
+print(f"Ingress established at {listener.url()}")
+
+# Keep the listener alive
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Closing listener")
