78910148953206e54ffce205004c80ca6289e4ca
alokv6109 <alokv6109@gmail.com>
2024-09-20 11:12:47 +0530

    first negative integers in array

---
+++ FirstNegativeInteger.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class FirstNegativeInteger {
+
+    public static long[] printFirstNegativeInteger(long arr[], int N, int k)
+    {
+        List<Long> ans = new ArrayList<>();
+        Queue<Long> q = new LinkedList<>();
+        int i=0, j=0;
+        while(j < arr.length){
+            if(arr[j] < 0){
+                q.offer(arr[j]);
+            }
+            if(j-i+1 < k){
+                j+= 1;
+            }else if(j-i+1== k){
+
+                if(q.size()>0){
+                    ans.add(q.peek());
+                }else{
+                    ans.add(0l);
+                }
+
+                if(q.size()>0 && arr[i] == q.peek()){
+                    q.poll();
+                }
+
+                i++;
+                j++;
+            }
+        }
+//    for getting the size of aray if it is required then n-k+1 is the size of the ans array can be remembered
+        return ans.stream().mapToLong(e -> e).toArray();
+    }
+
+    public static void main(String[] args) {
+        long arr[] = new long[]{-8, 2, 3, -6, 10};
+        int n = 5;
+        int k = 2;
+        long[] ans = printFirstNegativeInteger(arr, n, k);
+//        System.out.println(ans);
+        for (long i : ans){
+            System.out.println(i);
+        }
+    }
+}
