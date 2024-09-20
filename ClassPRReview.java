f2aa4226de8047d82dc68a8f6d233c98c5e37c6c
alokv6109 <alokv6109@gmail.com>
2024-09-20 10:49:38 +0530

    new PR review changes

---
+++ ClassPRReview.java
@@ -0,0 +1,7 @@
+public class ClassPRReview {
+    public static void main(String[] args) {
+        String newString = "This is just a example method";
+        String newExampleString  = newString;
+        System.out.println(newExampleString);
+    }
+}
